b98245b9aa97cad5974444b85a2acae7af9cd909
Ashraful Islam <ashraful0103@gmail.com>
2019-12-14 18:17:21 +0600

    Initial version that can rename by numeric serial

---
+++ FilesManipulator/FilesManipulator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FilesManipulator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProgressBar progressBar = new ProgressBar();
+            StringSanitizer stringSanitizer = new StringSanitizer();
+
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Files");//Assuming Test is your Folder
+
+            try
+            {
+                FileInfo[] Files = d.GetFiles("*.pdf"); //Getting Text files
+                string str = "", srcFile = "", destFile = "";
+                int counter = 1, totalfiles = Files.Length;
+
+                Console.WriteLine("Renaming files..");
+
+                foreach (FileInfo file in Files)
+                {
+                    Thread.Sleep(50);
+                    str = file.Name;
+
+                    //removing extras
+                    str = stringSanitizer.GetFirstAlphabetIndex(str, str.Length);
+
+                    srcFile = d + "\\" + file.Name;
+                    destFile = d + "\\" + counter.ToString() + "." + str;
+
+                    System.IO.File.Move(srcFile, destFile);
+                    //Console.WriteLine(str);
+                    //Console.Clear();
+
+                    progressBar.DrawProgressBar(counter, totalfiles);
+                    counter++;
+
+                }
+
+                Console.WriteLine("File renaming succesful");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
---
+++ FilesManipulator/FilesManipulator/StringSanitizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilesManipulator
+{
+    public class StringSanitizer
+    {
+        public string GetFirstAlphabetIndex(string str, int len)
+        {
+            int j, index = 0;
+
+
+            for (j = 0; j < len; j++)
+            {
+                if ((str[j] >= '0' && str[j] <= '9') || str[j] == '.')
+                {
+                    index = j;
+                }
+                else
+                {
+                    index++;
+                    break;
+                }
+            }
+
+            return str.Remove(0, index);
+        }
+    }
+}
